899bc9c0a409b8b92b82eca0438537be0358899b
QiaoXiaofeng <709404068@qq.com>
2020-03-23 23:22:53 +0800

    暂时删除

--- src/api/index.js
+++ src/api/index.js
@@ -14,7 +14,6 @@ export const reqCategorys = () => ajax(BASE_URL + '/index_category');
 // 3、根据经纬度获取商铺列表
 export const reqShops = ({longitude, latitude}) => ajax(BASE_URL + '/shops', {longitude, latitude});
 // 4、根据经纬度和关键字搜索商铺列表
-export const reqShopsByKeywords = ({longitude, latitude, keywords}) => ajax(BASE_URL + '/reqShopsByKeywords', {longitude, latitude, keywords});
 // 5、获取一次性验证码
 // 6、用户名密码登陆
 // 7、发送短信验证码
