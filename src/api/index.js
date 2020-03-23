4faf642da565a6b3c657622b5870b9fed3aa04ad
QiaoXiaofeng <709404068@qq.com>
2020-03-23 23:22:28 +0800

    qxfwebstorm

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,20 @@
 /* 请求接口模块
 返回Promise对象 */
 import ajax from './ajax'
+
 const BASE_URL = './api';
+
 // 1、根据经纬度获取位置详情
 export function reqAddress(geohash) {
-    return ajax(BASE_URL + `/position/${geohash}`);
+  return ajax(BASE_URL + `/position/${geohash}`);
 }
+
 // 2、获取食品分类列表
 export const reqCategorys = () => ajax(BASE_URL + '/index_category');
 // 3、根据经纬度获取商铺列表
-export const reqShops = ({ longitude, latitude }) => ajax(BASE_URL + '/shops', { longitude, latitude });
+export const reqShops = ({longitude, latitude}) => ajax(BASE_URL + '/shops', {longitude, latitude});
 // 4、根据经纬度和关键字搜索商铺列表
+export const reqShopsByKeywords = ({longitude, latitude, keywords}) => ajax(BASE_URL + '/reqShopsByKeywords', {longitude, latitude, keywords});
 // 5、获取一次性验证码
 // 6、用户名密码登陆
 // 7、发送短信验证码
